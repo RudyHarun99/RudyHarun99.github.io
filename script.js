ba175224c477a7fc53f95d2d37ab6322c1728853
Rudy Harun Arrasyid <rdyhrn@gmail.com>
2024-07-01 15:21:06 +0700

    add log

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const tombol = document.getElementById("tombol");
 
 tombol.addEventListener("click", () => {
+  console.log('clicked');
   const hello = document.getElementById("hello");
   hello.style.backgroundColor = "black";
   hello.style.color = "white";
