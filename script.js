6417462b715a96772c1c8bf551fd5b95521e1f81
Rudy Harun Arrasyid <57350097+RudyHarun99@users.noreply.github.com>
2024-07-29 16:26:03 +0700

    add list

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ tombol.addEventListener("click", () => {
   hello.style.backgroundColor = "black";
   hello.style.color = "white";
 })
+
+const data = [
+  {
+    id: 1,
+    name: 'cecep',
+    age: 20
+  },
+  {
+    id: 2,
+    name: 'maman',
+    age: 21
+  },
+  {
+    id: 3,
+    name: 'neneng',
+    age: 22
+  }
+]
+
+const list = document.getElementById('list');
+let content = '';
+for (let i = 0; i < data.length; i++) {
+  content += `<ul>${data[i].name}, ${data[i].age}</ul>`
+}
+list.innerHTML = content;
