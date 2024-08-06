8e35fc21f77f09c147cfa6274a7fcbcbf3c1c9c9
Rudy Harun Arrasyid <57350097+RudyHarun99@users.noreply.github.com>
2024-08-06 16:34:54 +0700

    add hello to script.js

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
-const tombol = document.getElementById("tombol");
+const hello = document.getElementById('hello');
+console.log(hello);
+
+
+
+
+
+
+
+
+
+// const tombol = document.getElementById("tombol");
 // console.log(tombol);
 
-const data = [
-  {
-    id: 1,
-    name: 'cecep',
-    age: 20
-  },
-  {
-    id: 2,
-    name: 'maman',
-    age: 21
-  },
-  {
-    id: 3,
-    name: 'neneng',
-    age: 22
-  }
-]
+// const data = [
+//   {
+//     id: 1,
+//     name: 'cecep',
+//     age: 20
+//   },
+//   {
+//     id: 2,
+//     name: 'maman',
+//     age: 21
+//   },
+//   {
+//     id: 3,
+//     name: 'neneng',
+//     age: 22
+//   }
+// ]
 
-tombol.addEventListener("click", () => {
-  console.log('clicked');
-  const hello = document.getElementById("hello");
-  hello.style.backgroundColor = "black";
-  hello.style.color = "white";
+// tombol.addEventListener("click", () => {
+//   console.log('clicked');
+//   const hello = document.getElementById("hello");
+//   hello.style.backgroundColor = "black";
+//   hello.style.color = "white";
 
-  let list = document.getElementById('list');
-  let content = '';
-  for (let i = 0; i < data.length; i++) {
-    content += `<li>${data[i].name}, ${data[i].age}</li>`;
-  }
-  console.log(content);
-  list.innerHTML = content;
-})
+//   let list = document.getElementById('list');
+//   let content = '';
+//   for (let i = 0; i < data.length; i++) {
+//     content += `<li>${data[i].name}, ${data[i].age}</li>`;
+//   }
+//   console.log(content);
+//   list.innerHTML = content;
+// })
