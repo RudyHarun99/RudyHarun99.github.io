6b1e924e674d4cde26f78137b4bf52aa6f3953ec
Rudy Harun Arrasyid <57350097+RudyHarun99@users.noreply.github.com>
2024-09-23 20:33:09 +0700

    add list from data

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ tombol.addEventListener("click", () => {
   hello.style.backgroundColor = "black";
   hello.style.color = "white";
 
-  // let list = document.getElementById('list');
-  // let content = '';
-  // for (let i = 0; i < data.length; i++) {
-  //   content += `<li>${data[i].name}, ${data[i].age}</li>`;
-  // }
-  // console.log(content);
-  // list.innerHTML = content;
+  let list = document.getElementById('list');
+  let content = '';
+  for (let i = 0; i < data.length; i++) {
+    content += `<li>${data[i].name}, ${data[i].age}</li>`;
+  }
+  console.log(content);
+  list.innerHTML = content;
 })
